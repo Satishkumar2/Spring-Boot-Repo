e6645bfac65a95d90f5e5ccea01e2a7dce9f6c50
Satishkumar2 <satish.b.kumar@capgemini.com>
2016-12-23 15:22:40 +0530

    Initial commit

---
+++ das-boot/src/main/java/com/cg/repository/ShipwreckRepository.java
@@ -0,0 +1,9 @@
+package com.cg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cg.model.Shipwreck;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long>{
+
+}
